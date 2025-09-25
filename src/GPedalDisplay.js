39835d3cb212a787ff0149d760e76681b1179bcb
Renato <88577821+renatz76@users.noreply.github.com>
2025-09-25 15:07:28 +0200

    Update GPedalDisplay.js

--- src/GPedalDisplay.js
+++ src/GPedalDisplay.js
@@ -1,3 +1,5 @@
+//comments
+
 import {geocode, getPanoramaByLocation} from './lib/gmapPromises'; 
 import {timeout, dateFormat, hasStravaOauthTokens,
     getStravaOauthTokens, setStravaOauthTokens,
